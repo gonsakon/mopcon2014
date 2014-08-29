0ccc28b35a5733473e7d714eed9107e60d8fd23c
liao <gonsakon@gmail.com>
2014-08-29 21:59:55 +0800

    新聞樣式更新

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ fireapp_livescripts_dir = "livescripts" # by Fire.app
 fireapp_minifyjs_on_build = false # by Fire.app 
 fireapp_always_report_on_build = true # by Fire.app 
 output_style = :expanded # by Fire.app 
-relative_assets = false # by Fire.app 
+relative_assets = true # by Fire.app 
 line_comments = false # by Fire.app 
 sass_options = {:debug_info=>false} # by Fire.app 
 sourcemap = false # by Fire.app 
